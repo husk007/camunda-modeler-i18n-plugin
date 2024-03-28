e6c24b2f650a84bb1fd959d94d13074f627b90a6
husk007 <72927717+husk007@users.noreply.github.com>
2024-03-28 14:18:44 +0100

    Create dmn-js.js
    Polish translation

---
+++ client/bpmnjs-i18n-extension/languages/pl/dmn-js.js
@@ -0,0 +1,81 @@
+/**
+ * Copyright 2021 FlowSquad GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * This file contains the translated strings used in the dmn-js component.
+ * However, notice that these strings are currently not working. We are
+ * still investigating how to apply them.
+ */
+export default {
+'Activate the lasso tool': 'Aktywuj narzędzie lasso',
+    'Add Cell Description': 'Dodaj opis komórki',
+    'Add Input': 'Dodaj wejście',
+    'Add Input Column Left': 'Dodaj kolumnę wejściową z lewej',
+    'Add Input Column Right': 'Dodaj kolumnę wejściową z prawej',
+    'Add Output': 'Dodaj wyjście',
+    'Add Output Column Left': 'Dodaj kolumnę wyjściową z lewej',
+    'Add Output Column Right': 'Dodaj kolumnę wyjściową z prawej',
+    'Add Predefined Values': 'Dodaj predefiniowane wartości',
+    'Add Rule Above': 'Dodaj regułę powyżej',
+    'Add Rule Below': 'Dodaj regułę poniżej',
+    'Add Values': 'Dodaj wartości',
+    'And': 'I',
+    'Annotations': 'Anotacje',
+    'Append {type}': 'Dołącz {type}',
+    'cellInput': 'Wejście komórki',
+    'Change Cell Expression Language': 'Zmień język wyrażenia komórki',
+    'Change type': 'Zmień typ',
+    'Clear predefined values.': 'Wyczyść predefiniowane wartości.',
+    'Copy Input Column': 'Skopiuj kolumnę wejściową',
+    'Copy Output Column': 'Skopiuj kolumnę wyjściową',
+    'Copy Rule': 'Skopiuj regułę',
+    'Create Decision': 'Utwórz decyzję',
+    'Create Input Data': 'Utwórz dane wejściowe',
+    'Create Knowledge Model': 'Utwórz model wiedzy',
+    'Create Knowledge Source': 'Utwórz źródło wiedzy',
+    'Cut Input Column': 'Wytnij kolumnę wejściową',
+    'Cut Output Column': 'Wytnij kolumnę wyjściową',
+    'Cut Rule': 'Wytnij regułę',
+    'Edit String': 'Edytuj ciąg znaków',
+    'Expression': 'Wyrażenie',
+    'Expression Language': 'Język wyrażenia',
+    'Input': 'Wejście',
+    'Input Expression': 'Wyrażenie wejściowe',
+    'Input Label': 'Etykieta wejścia',
+    'Input Type': 'Typ wejścia',
+    'Input Values': 'Wartości wejściowe',
+    'Input Variable': 'Zmienna wejściowa',
+    'No values': 'Brak wartości',
+    'Output Label': 'Etykieta wyjścia',
+    'Output Name': 'Nazwa wyjścia',
+    'Output Type': 'Typ wyjścia',
+    'Output Values': 'Wartości wyjściowe',
+    'Paste Input Column Left': 'Wklej kolumnę wejściową z lewej',
+    'Paste Input Column Right': 'Wklej kolumnę wejściową z prawej',
+    'Paste Output Column Left': 'Wklej kolumnę wyjściową z lewej',
+    'Paste Output Column Right': 'Wklej kolumnę wyjściową z prawej',
+    'Paste Rule Above': 'Wklej regułę powyżej',
+    'Paste Rule Below': 'Wklej regułę poniżej',
+    'Predefined Values': 'Wartości predefiniowane',
+    'Remove': 'Usuń',
+    'Remove Cell Description': 'Usuń opis komórki',
+    'Remove Input Column': 'Usuń kolumnę wejściową',
+    'Remove Output Column': 'Usuń kolumnę wyjściową',
+    'Remove Rule': 'Usuń regułę',
+    'Set Value': 'Ustaw wartość',
+    'Strings must be in double quotes.': 'Ciągi znaków muszą być w podwójnych cudzysłowach.',
+    'Type': 'Typ',
+};
