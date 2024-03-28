68966a2a9b1e0c75cd4280332c9f02cd06689a21
husk007 <72927717+husk007@users.noreply.github.com>
2024-03-28 14:17:13 +0100

    Create bpmn-js.js
    Polish translation

---
+++ client/bpmnjs-i18n-extension/languages/pl/bpmn-js.js
@@ -0,0 +1,80 @@
+/**
+ * Copyright 2021 FlowSquad GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * This file contains the strings used in the bpmn-js module.
+ */
+export default {
+'Append {type}': '{type} dołącz',
+  'Add Lane above': 'Dodaj Lane powyżej',
+  'Divide into two Lanes': 'Podziel na dwie Lane\'y',
+  'Divide into three Lanes': 'Podziel na trzy Lane\'y',
+  'Add Lane below': 'Dodaj Lane poniżej',
+  'Append ReceiveTask': 'Dołącz ReceiveTask',
+  'Append MessageIntermediateCatchEvent': 'Dołącz MessageIntermediateCatchEvent',
+  'Append TimerIntermediateCatchEvent': 'Dołącz TimerIntermediateCatchEvent',
+  'Append ConditionIntermediateCatchEvent': 'Dołącz ConditionIntermediateCatchEvent',
+  'Append SignalIntermediateCatchEvent': 'Dołącz SignalIntermediateCatchEvent',
+  'Append compensation activity': 'Dołącz aktywność kompensacyjną',
+  'Append EndEvent': 'Dołącz EndEvent',
+  'Append Gateway': 'Dołącz Gateway',
+  'Append Task': 'Dołącz Task',
+  'Append Intermediate/Boundary Event': 'Dołącz Zdarzenie Pośrednie/Graniczne',
+  'Change type': 'Zmień typ',
+  'Connect using Association': 'Połącz za pomocą Asocjacji',
+  'Connect using Sequence/MessageFlow or Association': 'Połącz za pomocą Sekwencji/Przepływu Wiadomości lub Asocjacji',
+  'Connect using DataInputAssociation': 'Połącz za pomocą DataInputAssociation',
+  'Remove': 'Usuń',
+  'no shape type specified': 'nie określono typu kształtu',
+  'out of bounds release': 'uwolnienie poza granicami',
+  'more than {count} child lanes': 'więcej niż {count} dziecięcych Lane\'ów',
+  'element required': 'wymagany element',
+  'no parent for {element} in {parent}': 'brak rodzica dla {element} w {parent}',
+  'Create {type}': 'Utwórz {type}',
+  'Activate the hand tool': 'Aktywuj narzędzie ręczne',
+  'Activate the lasso tool': 'Aktywuj narzędzie lasso',
+  'Activate the create/remove space tool': 'Aktywuj narzędzie tworzenia/usuwania przestrzeni',
+  'Activate the global connect tool': 'Aktywuj narzędzie globalnego łączenia',
+  'Create StartEvent': 'Utwórz StartEvent',
+  'Create Intermediate/Boundary Event': 'Utwórz Zdarzenie Pośrednie/Graniczne',
+  'Create EndEvent': 'Utwórz EndEvent',
+  'Create Gateway': 'Utwórz Gateway',
+  'Create Task': 'Utwórz Task',
+  'Create DataObjectReference': 'Utwórz DataObjectReference',
+  'Create DataStoreReference': 'Utwórz DataStoreReference',
+  'Create expanded SubProcess': 'Utwórz rozwinięty Podproces',
+  'Create Pool/Participant': 'Utwórz Pool/Uczestnik',
+  'Create Group': 'Utwórz Grupę',
+  'Parallel Multi Instance': 'Wielokrotne wystąpienie równoległe',
+  'Sequential Multi Instance': 'Wielokrotne wystąpienie sekwencyjne',
+  'Loop': 'Pętla',
+  'Ad-hoc': 'Ad-hoc',
+  'element {element} referenced by {referenced}#{property} not yet drawn': 'element {element} referencjonowany przez {referenced}#{property} jeszcze nie narysowany',
+  'unknown di {di} for element {semantic}': 'nieznane di {di} dla elementu {semantic}',
+  'missing {semantic}#attachedToRef': 'brakujący {semantic}#attachedToRef',
+  '{semantic}#{side} Ref not specified': '{semantic}#{side} Ref nie określony',
+  'already rendered {element}': 'już wyrenderowany {element}',
+  'failed to import {element}': 'nie udało się zaimportować {element}',
+  'multiple DI elements defined for {element}': 'zdefiniowano wiele elementów DI dla {element}',
+  'no bpmnElement referenced in {element}': 'brak odniesienia do bpmnElement w {element}',
+  'diagram not part of bpmn:Definitions': 'diagram nie jest częścią bpmn:Definitions',
+  'no diagram to display': 'brak diagramu do wyświetlenia',
+  'no process or collaboration to display': 'brak procesu lub współpracy do wyświetlenia',
+  'correcting missing bpmnElement on {plane} to {rootElement}': 'korygowanie brakującego bpmnElementu na {plane} do {rootElement}',
+  'unsupported bpmnElement for {plane}: {rootElement}': 'nieobsługiwany bpmnElement dla {plane}: {rootElement}',
+  'unrecognized flowElement {element} in context {context}': 'nierozpoznany flowElement {element} w kontekście {context}',
+  'HELLO {you}!': 'WITAJ {you}!'
+};
