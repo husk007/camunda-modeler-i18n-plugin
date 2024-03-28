46503c6489e1a7cfbc1c07638ed6d8a2db18df80
husk007 <72927717+husk007@users.noreply.github.com>
2024-03-28 14:19:30 +0100

    Create other.js
    Polish translation

---
+++ client/bpmnjs-i18n-extension/languages/pl/other.js
@@ -0,0 +1,97 @@
+/**
+ * Copyright 2021 FlowSquad GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * This file contains translations that were used in other components.
+ */
+export default {
+    'Open minimap': 'Otwórz minimapę',
+    'This maps to the process definition key.': 'To odnosi się do klucza definicji procesu.',
+    'Key': 'Klucz',
+    'Intermediate Throw Event': 'Zdarzenie Pośrednie Wyrzuć',
+    'End Event': 'Zdarzenie Końcowe',
+    'Message Start Event': 'Zdarzenie Rozpoczęcia Wiadomości',
+    'Timer Start Event': 'Zdarzenie Rozpoczęcia Timera',
+    'Conditional Start Event': 'Zdarzenie Rozpoczęcia Warunkowe',
+    'Signal Start Event': 'Zdarzenie Rozpoczęcia Sygnału',
+    'Expand (not reversible)': 'Rozwiń (nieodwracalne)',
+    'ID': 'ID',
+    'Start Event': 'Zdarzenie Rozpoczęcia',
+    'Message End Event': 'Zdarzenie Zakończenia Wiadomości',
+    'Escalation End Event': 'Zdarzenie Zakończenia Eskalacji',
+    'Error End Event': 'Zdarzenie Zakończenia Błędu',
+    'Compensation End Event': 'Zdarzenie Zakończenia Kompensacji',
+    'Signal End Event': 'Zdarzenie Zakończenia Sygnału',
+    'Terminate End Event': 'Zdarzenie Zakończenia Zakończenia',
+    'Transaction': 'Transakcja',
+    'Event Sub Process': 'Podproces Zdarzeń',
+    'Sub Process (collapsed)': 'Podproces (zwinięty)',
+    'Close minimap': 'Zamknij minimapę',
+    'Message Intermediate Catch Event': 'Zdarzenie Przechwytywania Wiadomości Pośredniej',
+    'Message Intermediate Throw Event': 'Zdarzenie Wyrzucenia Wiadomości Pośredniej',
+    'Timer Intermediate Catch Event': 'Zdarzenie Przechwytywania Timera Pośredniego',
+    'Escalation Intermediate Throw Event': 'Zdarzenie Wyrzucenia Eskalacji Pośredniej',
+    'Conditional Intermediate Catch Event': 'Zdarzenie Przechwytywania Warunkowego Pośredniego',
+    'Link Intermediate Catch Event': 'Zdarzenie Przechwytywania Pośredniego Połączenia',
+    'Link Intermediate Throw Event': 'Zdarzenie Wyrzucenia Pośredniego Połączenia',
+    'Compensation Intermediate Throw Event': 'Zdarzenie Wyrzucenia Kompensacji Pośredniej',
+    'Signal Intermediate Catch Event': 'Zdarzenie Przechwytywania Sygnału Pośredniego',
+    'Signal Intermediate Throw Event': 'Zdarzenie Wyrzucenia Sygnału Pośredniego',
+    'Parallel Gateway': 'Brama Równoległa',
+    'Inclusive Gateway': 'Brama Inkludowana',
+    'Complex Gateway': 'Brama Złożona',
+    'Event based Gateway': 'Brama Na Podstawie Zdarzeń',
+    'Exclusive Gateway': 'Brama Wyłączająca',
+    'Send Task': 'Zadanie Wyślij',
+    'Receive Task': 'Zadanie Odbierz',
+    'User Task': 'Zadanie Użytkownika',
+    'Manual Task': 'Zadanie Manualne',
+    'Business Rule Task': 'Zadanie Reguły Biznesowej',
+    'Service Task': 'Zadanie Usługi',
+    'Script Task': 'Zadanie Skryptowe',
+    'Call Activity': 'Wywołaj Aktywność',
+    'Sub Process (expanded)': 'Podproces (rozwinięty)',
+    'Sub Process': 'Podproces',
+    'Task': 'Zadanie',
+    'This maps to the task definition key.': 'To odnosi się do klucza definicji zadania.',
+    'Collapsed Pool': 'Zwinięty Basen',
+    'Expanded Pool': 'Rozwinięty Basen',
+    'flow elements must be children of pools/participants': 'elementy przepływu muszą być dziećmi basenów/uczestników',
+    'The follow up date as an EL expression (e.g. ${someDate} or an ISO date (e.g. 2015-06-26T09:54:00)': 'Data kontynuacji jako wyrażenie EL (np. ${someDate}) lub data ISO (np. 2015-06-26T09:54:00)',
+    'Message Boundary Event': 'Zdarzenie Graniczne Wiadomości',
+    'Timer Boundary Event': 'Zdarzenie Graniczne Timera',
+    'Escalation Boundary Event': 'Zdarzenie Graniczne Eskalacji',
+    'Conditional Boundary Event': 'Zdarzenie Graniczne Warunkowe',
+    'Error Boundary Event': 'Zdarzenie Graniczne Błędu',
+    'Cancel Boundary Event': 'Zdarzenie Graniczne Anulowania',
+    'Signal Boundary Event': 'Zdarzenie Graniczne Sygnału',
+    'Compensation Boundary Event': 'Zdarzenie Graniczne Kompensacji',
+    'Message Boundary Event (non-interrupting)': 'Zdarzenie Graniczne Wiadomości (bez przerywania)',
+    'Timer Boundary Event (non-interrupting)': 'Zdarzenie Graniczne Timera (bez przerywania)',
+    'Escalation Boundary Event (non-interrupting)': 'Zdarzenie Graniczne Eskalacji (bez przerywania)',
+    'Conditional Boundary Event (non-interrupting)': 'Zdarzenie Graniczne Warunkowe (bez przerywania)',
+    'Signal Boundary Event (non-interrupting)': 'Zdarzenie Graniczne Sygnału (bez przerywania)',
+    'Connect using Information/Knowledge/Authority Requirement or Association': 'Połącz za pomocą Wymagań Informacyjnych/Wiedzy/Uprawnień lub Asocjacji',
+    'Empty': 'Puste',
+    'Literal Expression': 'Wyrażenie Literałowe',
+    'This maps to the decision definition key.': 'To odnosi się do klucza definicji decyzji.',
+    'Decision Table': 'Tabela Decyzyjna',
+    'Output': 'Wyjście',
+    'Annotation': 'Anotacja',
+    'Output Expression': 'Wyrażenie Wyjściowe',
+    'Data Type': 'Typ Danych'
+
+}
